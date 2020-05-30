c7806a5a943a000ecb04b80841efc88a355ebe51
Andy Sanchez <andysanchez726@gmail.com>
2020-05-30 17:20:40 -0500

    Set a route to get notes, just displaying information, no styling

---
+++ server.js
@@ -0,0 +1,14 @@
+const express = require('express');
+const app = express();
+const PORT = process.env.PORT || 3001;
+const notes = require('./Develop/db/db');
+
+
+app.get('/notes', (req, res) => {
+    // let results = notes;
+    res.json(notes)
+});
+
+app.listen(PORT, () => {
+    console.log(`API server now on port ${PORT}!`);
+});
