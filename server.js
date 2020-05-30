c42ddc838955608a863279d60bb8239fe36b30ad
Andy Sanchez <andysanchez726@gmail.com>
2020-05-30 18:15:06 -0500

    homepage and notes page load. When notes page loads, the notes from db.json are displayed in the side bar and are clickable to edit

--- server.js
+++ server.js
@@ -1,12 +1,22 @@
 const express = require('express');
 const app = express();
+const path = require('path');
 const PORT = process.env.PORT || 3001;
 const notes = require('./Develop/db/db');
 
 
-app.get('/notes', (req, res) => {
+app.get('/api/notes', (req, res) => {
     // let results = notes;
     res.json(notes)
+});
+// app.use()
+app.use(express.static('./Develop/public'))
+// Routes to send html files
+app.get('/', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/index.html'));
+});
+app.get('/notes', (req, res) => {
+    res.sendFile(path.join(__dirname, './Develop/public/notes.html'));
 });
 
 app.listen(PORT, () => {
